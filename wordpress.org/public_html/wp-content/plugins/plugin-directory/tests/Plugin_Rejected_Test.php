d1c1d8b8853f23f066e7cfcc118fc0031b3433a1
davidperezgar <david@closemarketing.es>
2026-09-24 20:25:12 +0200

    Meet coding standards for rejection test

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/tests/Plugin_Rejected_Test.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/tests/Plugin_Rejected_Test.php
@@ -10,14 +10,22 @@ use WordPressdotorg\Plugin_Directory\Email\Plugin_Rejected;
 use WordPressdotorg\Plugin_Directory\Template;
 
 /**
+ * Tests plugin rejection emails.
+ *
  * @group email
  */
 class Plugin_Rejected_Test extends TestCase {
 
+	/**
+	 * Tests that scraping is available as a rejection reason.
+	 */
 	public function test_scraping_is_a_rejection_reason() {
 		$this->assertSame( 'Scraping', Template::get_rejection_reasons()['scraping'] );
 	}
 
+	/**
+	 * Tests that the scraping rejection email explains the applicable guideline.
+	 */
 	public function test_scraping_rejection_email_explains_the_guideline() {
 		$reflection = new ReflectionClass( Plugin_Rejected::class );
 		$email      = $reflection->newInstanceWithoutConstructor();
